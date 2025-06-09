3ed34b5eb1d108cf79658b2d010d111d0c1b88cb
Adyanullah <151485379+Adyanullah@users.noreply.github.com>
2025-06-09 22:34:34 +0700

    update migration

---
+++ app/Models/BarangRusak.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BarangRusak extends Model
+{
+    protected $guarded = ['id'];
+    public function barang()
+    {
+        return $this->belongsTo(Barang::class, 'barang_id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
---
+++ resources/views/inventaris/popup/rusak.blade.php
@@ -0,0 +1,22 @@
+<div class="modal fade" id="hapusModal" tabindex="-1" aria-labelledby="hapusModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <form class="modal-content" action="{{ route('inventaris.rusak') }}" method="POST">
+            @csrf
+            <div class="modal-header">
+                <h5 class="modal-title">Ajukan Barang Rusak</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Tutup"></button>
+            </div>
+            <div class="modal-body">
+                <input type="hidden" name="selected_ids" id="hapus-selected-ids" value="">
+                <input type="hidden" name="action_type" value="delete">
+                <div class="mb-3">
+                    <label for="keterangan_penghapusan" class="form-label">Keterangan</label>
+                    <textarea name="keterangan_penghapusan" id="keterangan_penghapusan" class="form-control" rows="3"></textarea>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-danger">Ajukan</button>
+            </div>
+        </form>
+    </div>
+</div>
